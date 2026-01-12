3960bcda676c81e99572cc6816ff04e43b4c4c82
Khusbu Kumari Jha <jkhusbu24@tbc.edu.np>
2026-01-12 10:53:15 +0545

    Delete Assignment_2 directory

--- Assignment_2/question_6.py
+++
@@ -1,8 +0,0 @@
-"""This program prompts the user for 10 integer inputs and stores only unique values in a list lastly, displays the sorted list of unique numbers at the end."""
-
-n = []
-for x in range(10):
-    i = int(input("Enter an integer:"))
-    if i not in n:
-        n.append(i)
-print("The final list:", n)
